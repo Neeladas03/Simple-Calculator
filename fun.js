855df5bf84eb0e952e35b1ad961b6078baac0dd6
Neeladhasan <neeladhasm@gmail.com>
2024-07-22 10:23:59 +0530

    first commit

---
+++ fun.js
@@ -0,0 +1,11 @@
+// function bulbon(){
+// document.getElementById('pic').src='pic_bulbon.gif';
+// }
+
+// function bulboff(){
+//     document.getElementById('pic').src='pic_bulboff.gif';
+// }
+
+// function forfun(){
+// document.getElementById("one").innerHTML="my name is neeladhas";}
+
